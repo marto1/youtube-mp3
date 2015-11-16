3372c299a8a815b0f19b3d78da97b13408ba0547
Martin Gergov <martingergov1@gmail.com>
2015-11-16 01:19:38 +0100

    file ending switch added

--- youtube-mp3.py
+++ youtube-mp3.py
@@ -3,13 +3,14 @@
 youtube-mp3.org utility.
 
 Usage: 
-youtube-mp3.py <url> <path>
+youtube-mp3.py [--autoend] <url> <path>
 
 Examples:
   youtube-mp3.py list
 
 Options:
   -h, --help
+  --autoend    auto append ".mp3" to the file if not present.
 """
 from docopt import docopt
 import requests
@@ -17,6 +18,23 @@ import time
 import sys
 import urllib
 import json
+
+MAIN="http://www.youtube-mp3.org"
+A="Mozilla/5.0 (X11; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0"
+ACC="text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+HEADERS = {
+    "Host": "www.youtube-mp3.org",
+    "User-Agent": A,
+    "Accept": ACC,
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Location": "*",
+    "Cache-Control": "no-cache",
+    "Referer": "http://www.youtube-mp3.org/",
+    "Connection": "keep-alive",
+    "Pragma": "no-cache",
+    }
+
 
 def sig(H):
     """Deobfuscated hashing"""
@@ -52,7 +70,7 @@ def sig(H):
          "/": 687,
          "=": 721}
     F = 1.51214
-    N, Y = 3219, 0
+    N = 3219
     H = unicode(H.lower(), "utf-8")
     for char in H:
         if char.isnumeric():
@@ -64,25 +82,9 @@ def sig(H):
     N = round(N * 1000)
     return int(N)
 
-
-MAIN="http://www.youtube-mp3.org"
-A="Mozilla/5.0 (X11; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0"
-ACC="text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-if __name__ == '__main__':
-    arguments = docopt(__doc__)
-    headers = {
-        "Host": "www.youtube-mp3.org",
-        "User-Agent": A,
-        "Accept": ACC,
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Location": "*",
-        "Cache-Control": "no-cache",
-        "Referer": "http://www.youtube-mp3.org/",
-        "Connection": "keep-alive",
-        "Pragma": "no-cache",
-        }
-    URL=urllib.quote_plus(arguments["<url>"])
+def download(url, filename, endwithmp3 = True):
+    """Fetch and save url to specified path."""
+    URL=urllib.quote_plus(url)
     form = "http://www.youtube-mp3.org/a/pushItem/?item={0}&el=na&bf=false"
     form += "&r={1}"
     form2 = "http://www.youtube-mp3.org/a/itemInfo/?video_id={0}"
@@ -90,12 +92,12 @@ if __name__ == '__main__':
     timestamp = int(time.time()*1000)
     rq = form.format(URL, timestamp)
     rq += "&s={0}".format(sig(rq))
-    r = requests.get(MAIN, headers=headers)
-    r = requests.get(rq, headers=headers)
+    r = requests.get(MAIN, headers=HEADERS)
+    r = requests.get(rq, headers=HEADERS)
     video_id = r.text
     rq = form2.format(video_id, timestamp)
     rq += "&s={0}".format(sig(rq))
-    r = requests.get(rq, headers=headers)
+    r = requests.get(rq, headers=HEADERS)
     json_data = json.loads(r.text[7:-1])
     ts_create = json_data[u'ts_create']
     h2 = json_data[u'h2']
@@ -104,10 +106,16 @@ if __name__ == '__main__':
     rq = form3.format(MAIN, video_id, ts_create, r_data, h2)
     rq += "&s={0}".format(sig(rq))
     r = requests.get(rq, stream=True)
-    filename = arguments["<path>"]
-    if not filename.endswith(".mp3"): #FIXME switch for that 
+    if not filename.endswith(".mp3") and endwithmp3:
         filename += ".mp3"
     with open(filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=2048): #FIXME option
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    download(arguments["<url>"],
+             arguments["<path>"],
+             arguments["--autoend"])
